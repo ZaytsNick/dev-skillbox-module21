97c20eeda7552956915f21c0a8dc02fe5043a409
zayts_home <10kcin01@gmail.com>
2024-02-13 00:46:06 +0500

    Task#4 wins evil

--- Tasl#4/main4.cpp
+++ Tasl#4/main4.cpp
@@ -1,54 +1,127 @@
 #include <iostream>
 #include <string>
 #include <random>
-
+#include <stdlib.h>
 struct person
 {
     std::string name;
-    int life = rand() % 151 + 50;
-    int armor = rand() % 51;
+    int position[2] = {rand() % 20, rand() % 20};
+    int life =  rand() % 151 + 50;
+    int armor =  rand() % 51;
     int damage = rand() % 31 + 15;
 };
 void creation_evil(char battlefield[][20], std::vector<person> &persons)
 {
     for (int i = 1; i <= 5; ++i)
     {
-        battlefield[rand() % 20][rand() % 20] = i;
         std::cout << "Enter name evil #" << i << ": ";
         std::cin >> persons[i].name;
     }
 }
 void creation_player(char battlefield[][20], std::vector<person> &persons)
 {
-    battlefield[rand() % 20][rand() % 20] = 0;
+
     std::cout << "Enter name Player: ";
     std::cin >> persons[0].name;
     std::cout << "Enter life, armor and damage:" << std::endl;
     std::cin >> persons[0].life >> persons[0].armor >> persons[0].damage;
 }
-void viewer_battlefield(char battlefield[][20])
+void viewer_battlefield(std::vector<person> &persons)
 {
     for (int i = 0; i < 20; ++i)
     {
         for (int j = 0; j < 20; ++j)
         {
-            if (battlefield[i][j] == 0)
-                std::cout << 'P';
+            bool tmp(false);
+            for (int k = 0; k < 6; ++k)
+            {
 
-            else if (battlefield[i][j] == -1)
+                if (persons[k].position[0] == i && persons[k].position[1] == j)
+                {
+                    if (k)
+                        std::cout << 'E';
+                    else
+                        std::cout << 'P';
+                    tmp = true;
+                    break;
+                }
+            }
+            if (!tmp)
                 std::cout << '*';
-            else
-                std::cout << 'E';
         }
         std::cout << std::endl;
     }
 }
-// enum spot
-// {
-//     player = 1,
-//     evil = 2,
-//     emptiness = 4
-// };
+bool fight(person person_1, person &person_2)
+{
+    if (person_2.armor - person_1.damage <= 0)
+    {
+        person_1.damage -= person_2.armor;
+        person_2.armor = 0;
+        if (person_2.life - person_1.damage <= 0)
+        {
+            system("cls");
+            std::cout << "Evil in the person of "<<person_2.name <<" is defeated" << std::endl;
+            return true;
+        }
+        else
+            person_2.life -= person_1.damage;
+    }
+    else
+    {
+        person_2.armor -= person_1.damage;
+    }
+    return false;
+}
+void is_there_anyone_here(int x, int y, std::vector<person> &persons)
+{
+    for (int i = 1; i < 6; ++i)
+    {
+        if (persons[i].position[0] == x && persons[i].position[1] == y)
+        {
+            if (fight(persons[0], persons[i]))
+            {
+                persons.erase(persons.begin() + i);
+                persons[0].position[0] = x;
+                persons[0].position[1] = y;
+                return;
+            }
+            else
+                return;
+            
+        }
+    }
+    persons[0].position[0] = x;
+    persons[0].position[1] = y;
+}
+
+void move(std::vector<person> &persons)
+{
+    char move;
+    while (move != '0')
+    {
+        std::cout << "W/A/S/D? ";
+        std::cin >> move;
+        switch (move)
+        {
+        case 'W':
+            is_there_anyone_here(((persons[0].position[0]) - 1), (persons[0].position[1]), persons);
+            break;
+        case 'A':
+            is_there_anyone_here((persons[0].position[0]), ((persons[0].position[1]) - 1), persons);
+            break;
+        case 'S':
+            is_there_anyone_here(((persons[0].position[0]) + 1), (persons[0].position[1]), persons);
+            break;
+        case 'D':
+            is_there_anyone_here((persons[0].position[0]), ((persons[0].position[1]) + 1), persons);
+            break;
+        default:
+            break;
+        }
+        viewer_battlefield(persons); 
+    }
+}
 int main()
 {
     char battlefield[20][20];
@@ -62,5 +135,6 @@ int main()
     std::vector<person> persons(6);
     creation_evil(battlefield, persons);
     creation_player(battlefield, persons);
-    viewer_battlefield(battlefield);
+    viewer_battlefield(persons);
+    move(persons);
 }
